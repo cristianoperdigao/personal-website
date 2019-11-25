5c575e1b02a0e8085747007e2db1c154268df44b
Cristiano Perdigão <cristianodpp@gmail.com>
2019-11-25 20:27:26 +0000

    feat(personal-website): create the initial react project.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import HomepageImage from './components/HomepageImage';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HomepageImage />
+        <p>
+          My new website
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
