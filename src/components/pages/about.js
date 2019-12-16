c3e6baefed04c2b217b21b3031f352fb45b223ce
Cristiano <cristianodpp@gmail.com>
2019-12-16 03:43:58 +0000

    Improving design about background and text about page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -11,17 +11,12 @@ class About extends Component {
                 </Animated>
                 <Animated animationIn="fadeInUp" animationOut="fadeOut" animationInDelay="200">
                     <p>
-                        Hello! I'm Cristiano, a software engineer based in London, who enjoys building things that live on the internet. I develop exceptional websites and web apps that provide intuitive, pixel-perfect user interfaces with efficient and modern backends.
+                        Hello! I'm Cristiano, a Full-Stack Developer based in London, who enjoys building things that live on the internet. 
                     </p>
                 </Animated>
                 <Animated animationIn="fadeInUp" animationOut="fadeOut" animationInDelay="400">
                     <p>
-                        Shortly after studied from polytechnic institute of Setúbal, I joined the engineering team at Upstatement where I work on a wide variety of interesting and meaningful projects on a daily basis.
-                    </p>
-                </Animated>
-                <Animated animationIn="fadeInUp" animationOut="fadeOut" animationInDelay="800">
-                    <p>
-                        Here are a few technologies I've been working with recently:
+                        I develop exceptional websites and web apps that provide intuitive, pixel-perfect user interfaces with efficient and modern backends.
                     </p>
                 </Animated>
             </div>
