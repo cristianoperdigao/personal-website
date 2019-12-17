a9bcb05c2fbeab12485fe66a2b161313665dfa1e
Cristiano <cristianodpp@gmail.com>
2019-12-17 04:51:26 +0000

    updating content

--- src/components/pages/experience.js
+++ src/components/pages/experience.js
@@ -34,6 +34,12 @@ class Experience extends Component {
                             <Col sm={9}>
                                 <Tab.Content>
                                     <Tab.Pane eventKey="kwan">
+                                        KWAN Full-time <br/>
+                                        Dates Employed: Nov 2016 – Dec 2019 <br/>
+                                        Employment Duration: 3 yrs 2 mos <br/>
+                                        Location: Lisbon Area, Portugal <br/><br/>
+
+
                                         - Backend development with PHP; <br/>
                                         - Database modeling; <br />
                                         - SOAP and REST Api modeling and development; <br />
@@ -44,6 +50,12 @@ class Experience extends Component {
                                         - Frontend development with Angular;
                                     </Tab.Pane>
                                     <Tab.Pane eventKey="igame">
+                                        iGame Full-time<br />
+                                        Dates Employed: Jun 2016 – Nov 2016<br />
+                                        Employment Duration: 6 mos<br />
+                                        Location: St Julian's, Malta<br /><br />
+
+
                                         - Developing high-quality and easily scalable websites in several markets worldwide; <br />
                                         - Focus on implementation of interactive user interfaces, including mobile, using current web technologies; <br />
                                         - Improving look and feel of sites as well as new features in backoffice; <br />
@@ -58,6 +70,12 @@ class Experience extends Component {
                                         - Converting static designs into live web pages and animations; <br />
                                     </Tab.Pane>
                                     <Tab.Pane eventKey="bit8">
+                                        Bit8 Full-time<br />
+                                        Dates Employed: Nov 2015 – May 2016<br />
+                                        Employment Duration: 7 mos<br />
+                                        Location: Gzira, Malta<br /><br />
+
+
                                         - Development of features on products to the company using PHP 5.6, JavaScript, HTML5, CSS and SQL; <br />
                                         - Providing maintenance, updates and bug fixes for live projects; <br />
                                         - Production, research and development of application software run on a MySQL; <br />
@@ -66,6 +84,11 @@ class Experience extends Component {
                                         - The post mainly involves development on the bit8 platform frontend, user interfaces, and game engine; <br />
                                     </Tab.Pane>
                                     <Tab.Pane eventKey="futureway">
+                                        Futureway Full-time<br />
+                                        Dates Employed: Oct 2013 – Oct 2015<br />
+                                        Employment Duration: 2 yrs 1 mo<br />
+                                        Location: Setubal, Portugal<br /><br />
+                                        
                                         - Developing frontend and backend of platform to support lawyers and all features about environment to them including control payments, time, files, contacts and legal actions; <br />
                                         - Creating the backend using PHP 5.4.x based on MVC; <br />
                                         - Creating the architecture of database system; <br />
@@ -73,6 +96,12 @@ class Experience extends Component {
                                         - Using PHP, JavaScript, HTML5, CSS3, MySQL; <br />
                                     </Tab.Pane>
                                     <Tab.Pane eventKey="simbiotic">
+
+                                        Simbiotic Full-time<br />
+                                        Dates Employed: May 2012 – Oct 2013<br />
+                                        Employment Duration: 1 yr 6 mos<br />
+                                        Location: Lisbon Area, Portugal<br /><br />
+
                                         - Cooperating with Business and UI/UX professionals; <br />
                                         - Converting layout design with HTML, CSS, Javascript; <br />
                                         - Implementing database structure; <br />
