9755d458b3928084bf2ba23cfc24a74b52438999
Cristiano <cristianodpp@gmail.com>
2019-12-17 04:29:51 +0000

    updating

--- src/components/helpers/topbar.js
+++ src/components/helpers/topbar.js
@@ -4,7 +4,7 @@ import { Navbar, Nav } from 'react-bootstrap';
 function LayoutTopbar() {
     return (
         <Navbar collapseOnSelect expand="lg" variant="dark">
-            <Navbar.Brand className="custom-logo" href="/">Perdigao <span>🚀</span></Navbar.Brand>
+            <Navbar.Brand className="custom-logo" href="/"><h1>_🚀</h1></Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto"></Nav>
--- src/components/pages/landing.js
+++ src/components/pages/landing.js
@@ -16,10 +16,10 @@ class Landing extends Component {
                         <h3 className="mt-5">I build things for the web.</h3>
                     </Animated>
                     <Animated animationIn="fadeInUp" animationOut="fadeOut" animationInDelay="900">
-                        <p className="mt-3">I'm a Full-Stack Developer based in London, with passion in building (and occasionally designing) exceptional, high-quality websites and applications.</p>
+                        <p className="mt-3">Currently based in London, UK as Full-Stack Developer.</p>
                     </Animated>
                     <Animated animationIn="bounceIn" animationInDelay="1500">
-                        <Button className="mt-5" variant="outline-warning">Let's talk</Button>
+                        <Button className="mt-3" href="https://www.linkedin.com/in/cristianoperdigao/" variant="outline-warning">Let's talk</Button>
                     </Animated>
                 </Col>
             </Row>
